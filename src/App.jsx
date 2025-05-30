85d2b6767a5e87e76ec17405baa5feb3694211e9
Minh Vương Quang <vuongquangminh120802@gmail.com>
2025-05-30 16:34:09 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Chatbot</h2>
+      <p>Xin chào! Tôi là chatbot</p>
+    </div>
+  );
+}
+
+export default App;
