5915236fa1e579fe12a597fa4a6b79298642a9e4
Minh Vương Quang <vuongquangminh120802@gmail.com>
2025-05-31 11:14:24 +0700

    test useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div style={{ padding: 20 }}>
       <h2>Chatbot</h2>
       <p>Xin chào! Tôi là chatbot</p>
+      <p>{count}</p>
+      <input
+        type="text"
+        onClick={() => {
+          console.log("123");
+          setCount((prev) => prev + 1);
+        }}
+      />
     </div>
   );
 }
